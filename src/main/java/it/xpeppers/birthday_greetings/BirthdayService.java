e6832356d146e55136975cb777a41656f56250a4
tonylook <antonio.alvino@surevive.ch>
2024-08-21 15:36:30 +0200

    separated birthday check from file reading

--- src/main/java/it/xpeppers/birthday_greetings/BirthdayService.java
+++ src/main/java/it/xpeppers/birthday_greetings/BirthdayService.java
@@ -11,16 +11,22 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BirthdayService {
 
 	public void sendGreetings(String fileName, XDate xDate, String smtpHost, int smtpPort) throws IOException, ParseException, AddressException, MessagingException {
+		List<Employee> employees = new ArrayList<>();
 		BufferedReader in = new BufferedReader(new FileReader(fileName));
 		String str = "";
 		str = in.readLine(); // skip header
 		while ((str = in.readLine()) != null) {
 			String[] employeeData = str.split(", ");
 			Employee employee = new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3]);
+			employees.add(employee);
+		}
+		for (Employee employee : employees) {
 			if (employee.isBirthday(xDate)) {
 				String recipient = employee.getEmail();
 				String body = "Happy Birthday, dear %NAME%!".replace("%NAME%", employee.getFirstName());
